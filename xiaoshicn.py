6faafda9d315f39b69df4ff6791bf1fbb1f12626
heikeling <110018283+heikeling@users.noreply.github.com>
2022-10-13 21:10:47 +0800

    Update xiaoshicn.py

--- xiaoshicn.py
+++ xiaoshicn.py
@@ -1,9 +1,9 @@
 #本库开发者为小诗编程
 def 打印(ptxt) :
-    print(ptxt)
+    print(ptxt)
 def 全局(variable) :
-    global _variable
+    global _variable
 def 获取输入(clew):
-    input(clew)
+    input(clew)
 def 初始化列表(txt,indexes):
-    [txt, indexes]
+    [txt, indexes]
